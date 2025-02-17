ad5d3cfe31c2b878ba35859a4db6d5dde86c57e2
zoe <zoe27@126.com>
2025-02-17 22:23:42 +0800

    视频音频下载

---
+++ ai_flow/VideoFetch/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+import os
+import logging
+import re
+
+from yt_dlp import YoutubeDL
+
+app = Flask(__name__)
+
+SAVE_PATH = "./save"
+
+
+def download(link, save_path, key):
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(save_path, f'{key}.mp3'),
+            'noplaylist': True,  # S'assure de ne pas traiter les playlists
+            'cookiefile': '/Users/apple/Documents/pythonPro/aivideo/ai_flow/VideoFetch/cookies.txt'
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            # Extraire les informations sans télécharger
+            info_dict = ydl.extract_info(link, download=False)
+            duration = info_dict.get('duration', 0)  # Durée en secondes
+
+            # Vérifier si la durée est supérieure à 20 minutes (1200 secondes)
+            if duration > 3600:
+                return None
+
+            # Si la durée est acceptable, télécharger
+            ydl.download([link])
+            video_title = info_dict.get('title', None)
+
+        print("Download is completed successfully")
+        return video_title
+    except Exception as e:
+        logging.error("An error occurred while downloading the audio: %s", e)
+        return None
+
+
+def validator_url(url):
+    if not url or url.isspace():
+        return False
+
+    youtube_pattern = re.compile(r'^(http|https)://(www\.)?youtube\.com/watch\?v=|^(http|https)://youtu\.be/')
+    if not youtube_pattern.match(url):
+        return False
+
+    return True
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    title = download("https://www.youtube.com/watch?v=T6wXCqO5tdY", "file", "key")
