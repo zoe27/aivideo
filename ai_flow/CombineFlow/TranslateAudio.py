2aecaf1bab5f97db64d300c68a46fa65c73bf650
zoe <zoe27@126.com>
2025-02-22 09:10:34 +0800

    生成语音的参数可传入

--- ai_flow/CombineFlow/TranslateAudio.py
+++ ai_flow/CombineFlow/TranslateAudio.py
@@ -18,21 +18,20 @@ def translate_text(text):
     model_path = "../Translation/m2m100-local"
     tokenizer = M2M100Tokenizer.from_pretrained(model_path)
     model = M2M100ForConditionalGeneration.from_pretrained(model_path)
-    tokenizer.src_lang = "zh"
+    tokenizer.src_lang = "en"
     inputs = tokenizer(text, return_tensors="pt")
-    generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.get_lang_id("en"))
+    generated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.get_lang_id("zh"))
     translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
     return translated_text
 
-async def text_to_speech(text, output_file):
-    voice = "en-US-AriaNeural"  # 选择微软的英文女声
+async def text_to_speech(text, output_file, voice="zh-CN-XiaoyiNeural"):  # 默认选择微软的中文女声
     tts = edge_tts.Communicate(text, voice)
     await tts.save(output_file)
     os.system(f"afplay {output_file}")  # macOS 播放音频
 
 async def main():
     # 1. 提取字幕文本
-    chinese_text = extract_subtitles("../Voice2Text/output.srt")
+    chinese_text = extract_subtitles("../Voice2Text/c56a6a5a785f6789688c067d99e1e504.srt")
     print("提取的字幕文本:", chinese_text)
 
     # 2. 翻译字幕文本
